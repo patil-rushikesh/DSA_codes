603a6e2c3b63d3a2bdf484655d0b2c555e224cf8
Rushikesh Patil <prushikeh@gmail.com>
2024-01-09 21:57:01 +0530

    Added an example for quadratic space and time complexity

--- Introduction/quadratic_space_time_complexity.cpp
+++ Introduction/quadratic_space_time_complexity.cpp
@@ -4,11 +4,15 @@ int main()
 {
     int arr[] = {1,2,3,4,5,6,7,10,20,0};
     int target = 10;
-    for(int  i = 0; i< sizeof(arr) / sizeof(arr[0]) ; i++){
-        for(int  j = i + 1; j < sizeof(arr) / sizeof(arr[0]) ; j++){
+    for(int  i = 0; i< sizeof(arr) / sizeof(arr[0]) ; i++){         //time complexity  O(n)
+
+        for(int  j = i + 1; j < sizeof(arr) / sizeof(arr[0]) ; j++){    //time complexity O(n)
             if(arr[i] + arr[j] == target){
                 cout<<"Pair "<<arr[i]<<"+"<<arr[j]<<endl;
             }
         }
     }
+
+    //Overall time complexity
+    //O(n)* O(n) = O(n^2)
 }
