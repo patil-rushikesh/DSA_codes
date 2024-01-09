1fbf51a6e61c3ae1e535ae6b04a54a043e5f1ec7
Rushikesh Patil <prushikeh@gmail.com>
2024-01-09 20:49:04 +0530

    Added an example for linear space and time complexity

--- Introduction/constant_space_time_complexity.cpp
+++ Introduction/constant_space_time_complexity.cpp
@@ -5,7 +5,7 @@ int main(){
     int arr[] = {1,2,3,4,5};
     //add 10 to first element in the array
     arr[0] = arr[0] + 10; //time complexity -> O(1)
-    int num = arr[sizeof(arr) - 1];  //space complexity - int 4 bytes O(4)
+    int num = arr[sizeof(arr) / sizeof(arr[0]) - 1];  //space complexity - int 4 bytes O(4)
     cout<<arr[0];
     cout<<num;
     //O(1) + O(1) = O(2) == O(1)  //constant time complexity
