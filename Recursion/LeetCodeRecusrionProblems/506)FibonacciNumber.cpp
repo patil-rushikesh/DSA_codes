b530871fd45bf8a5a14e1b8947b8408d53cb2c4f
Rushikesh Patil <prushikeh@gmail.com>
2024-03-23 17:04:08 +0530

    Added Leetcode Recursion Problems

---
+++ Recursion/LeetCodeRecusrionProblems/506)FibonacciNumber.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+
+public:
+    int fib(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return fib(n - 2) + fib(n - 1);
+        }
+    }
+};
--- Recursion/power.cpp
+++ Recursion/power.cpp
@@ -7,6 +7,13 @@ int pow(int m, int n){
         return pow(m,n-1)*m;
     }
 }
+int pow(int m, int n){
+    if(n == 0){
+        return 1;
+    }else{
+        return pow(m,n-1)*m;
+    }
+}
 int main(){
     int x,y;
     cout<<"Enter Base : ";
